f669344e57bfa8c489e203fb7c7bda8ed3e084a8
Clint Bush <clint@madebyclint.com>
2023-10-24 19:30:41 -0700

    Add counter

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ const cards: Card[] = data.cards as any;
 const deckReference = buildDeckReference(cards);
 const deck = buildDeck(deckReference);
 const deckShuffled = shuffleArray(deck);
+let deckCount = deckShuffled.length;
 
 const renderedDeck = renderDeck(deckShuffled, deckReference);
 
@@ -23,6 +24,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div id="draw-pile" class="card-placeholder">Draw pile</div>
     <div id="discard-pile" class="card-placeholder">Discard pile</div>
   </div>
+  <div id="counter" class="pile-counter"><span id="count" class="count">${deckCount}</span> cards left</div>
 `;
 
 const drawPile = document.getElementById("draw-pile");
@@ -30,12 +32,24 @@ renderedDeck.forEach((card) => {
     drawPile?.appendChild(card);
 });
 
+const discardPile = document.getElementById("discard-pile");
+drawPile?.addEventListener("click", discard);
+
+function updateCount() {
+    deckCount -= 1;
+    console.log("deckCount", deckCount);
+    const counter = document.getElementById("count") as HTMLDivElement;
+    counter.innerText = deckCount.toString();
+}
+
 function discard(e: Event) {
     const target = e.target as Element;
     const parent = target.closest(".card");
     moveCard(parent as HTMLDivElement, discardPile as HTMLDivElement);
+    updateCount();
+    if (deckCount === 0) {
+        alert("no more cards");
+        drawPile?.removeEventListener("click", discard);
+    }
     // TODO: Add stop here when out of cards
 }
-
-const discardPile = document.getElementById("discard-pile");
-drawPile?.addEventListener("click", discard);
