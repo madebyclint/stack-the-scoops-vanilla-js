7972e47a50ad3ec15121ca8c8a295f24a0c7152c
Clint Bush <clint@madebyclint.com>
2023-11-04 09:15:39 -0700

    Add toast notifications

--- src/utilities/move-controls.ts
+++ src/utilities/move-controls.ts
@@ -1,3 +1,6 @@
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
 import { constants } from "../appsettings";
 import { CardReference, updateCount } from "./deck-controls";
 
@@ -107,7 +110,21 @@ export function findEligibleAction(
     console.log("eligible-to-play", eligiblePlaysLeft);
     if (!initialSetup && eligiblePlaysLeft.length === 0) {
         discard(card, 0, 0, 0);
-        alert("nothing left to play");
+        Toastify({
+            // className: "toast",
+            text: "Nothing left to play",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function () {}, // Callback after click
+        }).showToast();
     }
 }
 
